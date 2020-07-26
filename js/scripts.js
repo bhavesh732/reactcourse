77576bcdbd44c12baa36f604cb7dec306dce7647
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-07-26 18:00:15 +0530

    npm scripts.1

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+        interval: 1000
+    });
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#myCarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#myCarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    $('#reserveTable').click(function() {
+        $('#reserveModal').modal('show');
+    });
+    $('#loginButton').click(function() {
+        $('#loginModal').modal('show');
+    });
+    $('.close').click(function() {
+        $('.modal').modal('hide');
+    });
+});
